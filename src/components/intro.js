be8becf259f300886b8d0e34e4fb62d9c15d29bf
sergihejo <70150143+sergihejo@users.noreply.github.com>
2024-10-08 22:49:25 +0200

    fix: responsive

--- src/components/intro.js
+++ src/components/intro.js
@@ -7,7 +7,7 @@ export default function Intro() {
 			<img
 				src="https://avatars.githubusercontent.com/u/70150143?v=4"
 				alt="Descripción de la imagen"
-				className="w-2/3 md:w-1/3 h-auto mr-4 md:mt-4 mb-12 rounded-full order-1 md:order-2"
+				className="w-2/3 md:w-1/3 h-auto md:mr-4 md:mt-4 mb-12 rounded-full order-1 md:order-2"
 			/>
 			<div className="md:mr-16 order-2 md:order-1">
 				<h3 className="text-4xl font-bold">¡Hola! Soy Sergio</h3>
